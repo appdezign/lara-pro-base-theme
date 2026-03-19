1aa27eb6e741f9541f00e2b6dade7372827bdf6e
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-03-19 21:16:10 +0100

    Fix home slider

--- views/_widgets/slider/home.blade.php
+++ views/_widgets/slider/home.blade.php
@@ -1,7 +1,7 @@
-<?php $heros = json_decode(json_encode(config('data.heros')), false); ?>
+@if($widgetsliders)
 
-<div class="js-swiper swiper swiper-nav-onhover"
-     data-swiper-options='{
+	<div class="js-swiper swiper swiper-nav-onhover"
+	     data-swiper-options='{
 				"effect": "fade",
 				"loop": true,
 				"speed": 800,
@@ -18,44 +18,48 @@
 					"nextEl": ".swiper-button-next"
 				}
 			}'>
-	<div class="swiper-wrapper lara-hero lara-hero-height" data-theme="dark">
+		<div class="swiper-wrapper lara-hero lara-hero-height" data-theme="dark">
 
-		@foreach($heros as $hero)
-			<!-- Hero Item start -->
-			<div class="swiper-slide h-full"
-			     style="background-image: url('{{ $hero->image }}');">
+			@foreach($widgetsliders as $widgetslider)
+				<!-- Hero Item start -->
+				<div class="swiper-slide h-full"
+				     style="background-image: url('{!! glideUrl($widgetslider->featured()->path, 1920, 960) !!}');">
 
-				<div class="hero h-full">
-					<div class="hero-overlay"></div>
-					<div class="hero-content text-center">
-						<div class="max-w-3xl">
-							<h2 class="heading mb-5 text-3xl md:text-4xl/12 lg:text-5xl/20 xl:text-6xl/20  font-bold from-start">
-								{{ $hero->title }}
-							</h2>
+					<div class="hero h-full">
+						<div class="hero-overlay"></div>
+						<div class="hero-content text-center">
+							<div class="max-w-3xl">
+								<h2 class="heading mb-5 text-3xl md:text-4xl/12 lg:text-5xl/20 xl:text-6xl/20  font-bold from-start">
+									{{ $widgetslider->title }}
+								</h2>
 
-							<div class="mb-12 px-16 lg:px-24 from-end">
-								<p>
-									{{ $hero->payoff }}
-								</p>
-							</div>
+								<div class="mb-12 px-16 lg:px-24 from-end">
+									<p>
+										{{ strip_tags($widgetslider->payoff) }}
+									</p>
+								</div>
 
-							<div class="scale-up delay-1s">
-								<button class="btn btn-lg btn-primary">{{ $hero->cta }}</button>
+								@if(!empty($widgetslider->url))
+									<div class="scale-up delay-1s">
+										<a href="{{ $widgetslider->url }}" class="btn btn-lg btn-primary">
+											{{ $widgetslider->urltitle }}
+										</a>
+									</div>
+								@endif
 							</div>
 						</div>
 					</div>
+
 				</div>
+				<!-- Hero Item end -->
+			@endforeach
 
-			</div>
-			<!-- Hero Item end -->
-		@endforeach
+			<div class="swiper-button-prev rounded-full bg-white/10 hover:bg-primary"></div>
+			<div class="swiper-button-next rounded-full bg-white/10 hover:bg-primary"></div>
 
+			<div class="swiper-pagination"></div>
 
-
-		<div class="swiper-button-prev rounded-full bg-white/10 hover:bg-primary"></div>
-		<div class="swiper-button-next rounded-full bg-white/10 hover:bg-primary"></div>
-
-		<div class="swiper-pagination"></div>
-
+		</div>
 	</div>
-</div>
+
+@endif
